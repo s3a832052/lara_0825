0ffdadf74b89288133fe5771f5485a9d71fcb5b0
GuoTingRui <azsxdc890902@gmail.com>
2021-11-07 23:25:55 +0800

    練習3:撰寫migration內容

--- database/migrations/2021_11_07_152155_create_posts_table.php
+++ database/migrations/2021_11_07_152155_create_posts_table.php
@@ -15,6 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('table');
+            $table->text('content');
             $table->timestamps();
         });
     }
