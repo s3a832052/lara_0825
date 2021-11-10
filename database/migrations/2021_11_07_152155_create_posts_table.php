86d7c8066a50019e60efe77072d159ad0b7b632e
GuoTingRui <azsxdc890902@gmail.com>
2021-11-10 15:36:51 +0800

    修正title拼字錯誤

--- database/migrations/2021_11_07_152155_create_posts_table.php
+++ database/migrations/2021_11_07_152155_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('table');
+            $table->string('title');
             $table->text('content');
             $table->timestamps();
         });
