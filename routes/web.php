3139f749f7346eb6ed5f7a1c76c07b2179bfc110
GuoTingRui <azsxdc890902@gmail.com>
2021-11-14 23:53:40 +0800

    練習2-4:新增create方法

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,15 @@ use App\Models\Post;
 
 Route::get('/', function () {
     //return view('welcome');
-    $post=new Post();
-    $post->title ='test title';
-    $post->content='test content';
-    $post->save();
+//    $post=new Post();
+//    $post->title ='test title';
+//    $post->content='test content';
+//    $post->save();
+
+    Post::create([
+        'title'=>'test title2',
+        'content'=>'test content2'
+    ]);
 });
 Route::get('post',[PostsController::class,'index'])->name('posts.index');
 Route::get('posts',[PostsController::class,'show'])->name('posts.show');
